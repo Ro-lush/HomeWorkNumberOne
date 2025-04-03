f900b4e0e7e4a64bbb93f6f07b9233b2760be911
romanlusnikov <aectann28@gmail.com>
2025-04-03 22:51:42 +0400

    add

--- Solution/src/main/java/org/example/Calculator.java
+++ Solution/src/main/java/org/example/Calculator.java
@@ -1,31 +1,46 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * Клас реализует выполнене базовые арифметические операции.
  */
-public  class Calculator {
-   final static String REGEX = "[^1234567890+-\\/^]";  //"[1234567890+-\\/^]";
-   final static String welcome = "Для вывода истории операции введите 'history', для отчистки итории введите 'clean'";
+public class Calculator {
+    final static String REGEX_OPERATION = "[^+-\\/^]";  //"[1234567890+-\\/^]";
+    final static String REGEX_NUMBER = "[^1234567890]";  //"[1234567890+-\\/^]";
+
+    final static String welcome = "Для вывода истории операции введите 'history', для отчистки итории введите 'clean'";
 
     /**
      * Метод для проверки вводимых значений
+     *
      * @param symbol - введеные даные.
      * @return - true or false
      */
-    public boolean checkSymbol(String symbol){
-        if (Pattern.matches(REGEX,symbol)){
-            System.out.println("Присутсвуют символы отличающие от арифмитических операций и чисел, введите снова");
+    public boolean checkSymbol(String symbol) {
+        final Pattern pattern = Pattern.compile(REGEX_OPERATION, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(symbol);
+
+        if (matcher.find()) {
+            System.out.println("Присутсвуют символы отличающие от арифмитических операций, введите снова");
         }
-        return Pattern.matches(REGEX,symbol);
+        return matcher.find();
+    }
+    public boolean checkNumbers(String symbol) {
+        final Pattern pattern = Pattern.compile(REGEX_NUMBER, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(symbol);
+
+        if (matcher.find()) {
+            System.out.println("Присутсвуют символы отличающие от чисел, введите снова");
+        }
+        return matcher.find();
     }
 
     public void start() {
         Scanner scanner = new Scanner(System.in);
         String operation = getOperation(scanner);
-        System.out.println(operation);
         /*double a = getNumber(scanner, "первое");
         double b = getNumber(scanner, "второе");*/
         scanner.close();
@@ -42,10 +57,10 @@ public  class Calculator {
         String operation;
         System.out.println("Введите '+' для сложения, '-' для вычитания, '*' для умножения, '/' для деления, " +
                 "для возведения в степень '^' : ");
-
-      do {
-          operation = scanner.nextLine();
-      }while (!checkSymbol(operation));
+        operation = scanner.nextLine();
+        while (checkSymbol(operation)){
+            operation = scanner.nextLine();
+        }
         return operation;
     }
 
